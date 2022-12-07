c8acc6ca94679153ec1942606dc2ec6afb8799b9
Dionei Silva <dionei.silva@gmail.com>
2022-12-07 10:14:40 -0300

    exercicio1-ok

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,11 @@
 /* 1. Crie uma variável para ler a idade de um motorista. Caso o
 motorista tenha idade maior ou igual a 18 anos imprime no console
 “Pode dirigir”, caso contrário imprima “Não pode dirigir”. */
+
+let idade = Number(prompt("Insira idade: "));
+
+if (idade < 18) {
+    console.log("Nao pode dirigir");
+} else {
+    console.log("pode dirigir");
+}
