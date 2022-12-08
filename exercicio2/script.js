837bdd0794134e0ce36e34eeeda585a942807ea6
Dionei Silva <dionei.silva@gmail.com>
2022-12-08 06:42:25 -0300

    exercicio2-ok

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -3,3 +3,15 @@ condicional, além de saber se o motorista tem 18 anos ou mais,
 temos que saber também se ele é habilitado para dirigir. Caso ele
 tenha idade maior ou igual a 18 anos e possua habilitação, insira no
 html “Pode dirigir” caso contrário imprima “Não pode dirigir”. */
+
+let idade = Number(prompt("Insira idade: "));
+let habilitado = prompt("É habilitado? ");
+let sim = true;
+let nao = false;
+if (idade < 18) {
+    console.log("Nao pode dirigir");
+} else if (idade >= 18 && habilitado === sim) {
+    console.log("Habilitado pode dirigir");
+} else {
+    console.log("nao habilitado, nao pode dirigir");
+}
